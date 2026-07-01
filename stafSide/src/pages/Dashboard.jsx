b12d355c3afdc3dc8b0351a4c932166654d35228
Vikas Jangid <vikasjangid3352@gmail.com>
2026-07-01 12:46:36 +0530

    chore: update CORS origins, rename portal title, and simplify chart container props

--- stafSide/src/pages/Dashboard.jsx
+++ stafSide/src/pages/Dashboard.jsx
@@ -2271,7 +2271,7 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="h-[300px] sm:h-[350px]">
-            <ResponsiveContainer width="100%" height="100%" minWidth={0} minHeight={0}>
+            <ResponsiveContainer width="100%" height="100%">
               <BarChart data={chartData} margin={{ top: 0, right: 0, left: -20, bottom: 0 }}>
                 <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#e2e8f0" />
                 <XAxis 
